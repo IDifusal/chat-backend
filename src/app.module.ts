2cabd53d88e59acf35880de171f6ce721d0fe5a6
Cristhian Sanchez <52178427+IDifusal@users.noreply.github.com>
2025-03-04 19:53:47 -0500

    comment out MongooseModule import in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GptModule } from './gpt/gpt.module';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     GptModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    // MongooseModule.forRoot(process.env.MONGO_URL),
   ],
 })
 export class AppModule {}
